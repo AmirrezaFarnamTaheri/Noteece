03b83a53e0505634c1cf3fb5d26e9f9871e116ad
Farnam <taheri.farnam@gmail.com>
2026-07-30 12:46:10 +0330

    test(mobile): remove obsolete React Native animated helper mock

--- apps/mobile/jest.pre-setup.js
+++ apps/mobile/jest.pre-setup.js
@@ -2,9 +2,6 @@
 import 'react-native-gesture-handler/jestSetup';
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
-// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-
 // Mock common native modules
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
